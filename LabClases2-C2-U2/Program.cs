177813124ecab83c62cb9b34034c135b4a97fd6b
Tiago Pieroni <tepieroni@gmail.com>
2023-05-15 16:19:10 -0300

    Agregar archivos de proyecto.

---
+++ LabClases2-C2-U2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Clases;
+using System;
+
+namespace LabClases2_C2_U2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            A a = b;
+            a.F();
+            b.F();
+            a.G();
+            b.G();
+            Console.ReadKey();
+
+        }
+    }
+}
